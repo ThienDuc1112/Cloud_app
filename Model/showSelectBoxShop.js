96ba60e6d5cd16155414de471d5b484f4fa0c8a6
@nv_truonqqq <95234772+ThienDuc1112@users.noreply.github.com>
2022-10-24 15:04:06 +0700

    Update

--- Model/showSelectBoxShop.js
+++ Model/showSelectBoxShop.js
@@ -1,23 +1,22 @@
 var pg_conn = require('./database');
 async function viewShop() {
-    const shop_query = {
-        text: `SELECT shop.name from shop inner join users on users.shop = shop.id where users.role = 'shop';`
-    }
-    var query_data = await pg_conn.query(shop_query)
-    var select_box_string = `<label for="shop">Choose a shop:</label>
+  const shop_query = {
+    text: `SELECT shop.name from shop inner join users on users.shop = shop.id where users.role = 'shop';`
+  }
+  var query_data = await pg_conn.query(shop_query)
+  var select_box_string = `<label for="shop">Choose a shop:</label>
     <select name="shop">
     <option value="all">all</option>`;
-    for(let i = 0; i<query_data.rows.length; i++){
-      select_box_string += `<option value="${query_data.rows[i].name}">${query_data.rows[i].name}</option>`;
-    }
-    select_box_string += `</select>`;
-    var form_select_box = `
+  for (let i = 0; i < query_data.rows.length; i++) {
+    select_box_string += `<option value="${query_data.rows[i].name}">${query_data.rows[i].name}</option>`;
+  }
+  select_box_string += `</select>`;
+  var form_select_box = `
     <form method="POST" action="/findShop"> 
-    <h3> Select a shop </h3>
     ${select_box_string}
     <button type="submit"> Search </button>
     </form>`
-    
-    return form_select_box;
+
+  return form_select_box;
 }
 module.exports = viewShop;
--- routes/index.js
+++ routes/index.js
@@ -14,13 +14,15 @@ router.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false,
-    httpOnly:true,
-  maxAge: 60000 }
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    maxAge: 60000
+  }
 }));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 /* GET login page. */
@@ -28,39 +30,39 @@ router.post('', function (req, res) {
   res.render('login');
 });
 /* route to user or admin page. */
-router.post('/login', async function(req, res, next){
+router.post('/login', async function (req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
   var sessionData = req.session;
-  let [authenticated, idshop, role] = await authen(username,password);
-      sessionData.user ={
-        username: username,
-        password: password,
-        idshop: idshop,
-      }
-      console.log(sessionData.shop);
-  if(authenticated==true&&role=='shop'){
+  let [authenticated, idshop, role] = await authen(username, password);
+  sessionData.user = {
+    username: username,
+    password: password,
+    idshop: idshop,
+  }
+  console.log(sessionData.shop);
+  if (authenticated == true && role == 'shop') {
     let productString = await tableProduct(idshop);
-    res.render('home',{products: productString});
-  } else if(authenticated==true&&role=='admin'){
+    res.render('home', { products: productString });
+  } else if (authenticated == true && role == 'admin') {
     let listSelectShop = await selectBoxShop();
     let tableProduct = await getTableSelectProduct('all');
-    res.render('admin',{selectBox: listSelectShop,result: tableProduct})
-  } else{
-    res.render('login', {notice: "wrong username or password"});
-} 
+    res.render('admin', { selectBox: listSelectShop, result: tableProduct })
+  } else {
+    res.render('login', { notice: "wrong username or password" });
+  }
 });
 
 router.post('/findShop', async function (req, res, next) {
- let name = req.body.shop;
+  let name = req.body.shop;
   let listSelectShop = await selectBoxShop();
   var tableProduct = await getTableSelectProduct(name);
-  res.render('admin', {Title:'Admin',selectBox: listSelectShop, result: tableProduct})
+  res.render('admin', { Title: 'Admin', selectBox: listSelectShop, result: tableProduct })
 });
 
-router.post('/button', async function(req, res, next){
+router.post('/button', async function (req, res, next) {
   var action = req.body.button;
-  if(action == "update"){
+  if (action == "update") {
     var id_product = req.body.id;
     var detail = await detailProduct(id_product);
     let detail_product_string = `
@@ -77,38 +79,38 @@ router.post('/button', async function(req, res, next){
   <button type="submit"> Save </button>
 </form>
     `;
-    res.render('updateProduct',{product_detail:detail_product_string});
-  } else{
+    res.render('updateProduct', { product_detail: detail_product_string });
+  } else {
     var shop = req.body.shop;
     var id_product = req.body.id;
-     deleteItem(id_product);
-     let productString = await tableProduct(shop);
-    res.render('home',{products: productString});
+    deleteItem(id_product);
+    let productString = await tableProduct(shop);
+    res.render('home', { products: productString });
   }
-  
+
 });
 
-router.post('/update', async function(req, res, next){
+router.post('/update', async function (req, res, next) {
   let idProduct = req.body.id;
   let nameProduct = req.body.name;
   let quantityProduct = req.body.quantity;
   let priceProduct = req.body.price;
   let shop = req.body.shop;
- 
-  await updateItem(idProduct,nameProduct,quantityProduct,priceProduct);
+
+  await updateItem(idProduct, nameProduct, quantityProduct, priceProduct);
   let productString = await tableProduct(shop);
-  res.render('home',{products: productString});
+  res.render('home', { products: productString });
 });
 
-router.post('/addProduct', async function(req, res, next){
+router.post('/addProduct', async function (req, res, next) {
   let id = req.body.id;
   let name = req.body.name;
   let quantity = req.body.quantity;
   let price = req.body.price;
   var idShop = req.session.user.idshop;
   console.log(req.session.user.idshop);
- addItem(id,name,quantity,price,idShop);
- let productString = await tableProduct(idShop);
- res.render('home',{products: productString});
+  addItem(id, name, quantity, price, idShop);
+  let productString = await tableProduct(idShop);
+  res.render('home', { products: productString });
 });
 module.exports = router;
